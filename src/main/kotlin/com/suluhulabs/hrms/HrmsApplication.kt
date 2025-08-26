b51a11e69ab198f8dcfe078ba90c8287f8b3f10c
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-08-26 15:51:37 +0300

    chore: install springdoc-openapi and io.jsonwebtoken dependancies

---
+++ src/main/kotlin/com/suluhulabs/hrms/HrmsApplication.kt
@@ -0,0 +1,11 @@
+package com.suluhulabs.hrms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrmsApplication>(*args)
+}
