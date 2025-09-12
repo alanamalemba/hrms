57c865429c35c0e7d8362dddafad3adbc2d9e670
Alan Amalemba <alanlsamalemba@gmail.com>
2025-09-12 09:43:53 +0300

    feat: rename 'jobposting' occurences with 'jobpost'

--- src/main/kotlin/com/suluhulabs/hrms/dto/CreateJobPostRequestDto.kt
+++ src/main/kotlin/com/suluhulabs/hrms/dto/CreateJobPostRequestDto.kt
@@ -3,7 +3,8 @@ package com.suluhulabs.hrms.dto
 import jakarta.validation.constraints.NotBlank
 import jdk.jfr.Description
 
-data class CreateJobPostingRequestDto(
+data class CreateJobPostRequestDto(
     @field:NotBlank val title: String,
     @field:NotBlank val description: String
 )
+
--- src/main/kotlin/com/suluhulabs/hrms/repository/JobPostRepository.kt
+++ src/main/kotlin/com/suluhulabs/hrms/repository/JobPostRepository.kt
@@ -1,25 +1,25 @@
 package com.suluhulabs.hrms.repository
 
-import com.suluhulabs.hrms.model.JobPosting
+import com.suluhulabs.hrms.model.JobPost
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
-interface JobPostingRepository : JpaRepository<JobPosting, Long> {
+interface JobPostingRepository : JpaRepository<JobPost, Long> {
 
     @Query("""
         SELECT jp
-        FROM job_postings jp
+        FROM job_posts jp
         WHERE (:query IS NULL OR 
               LOWER(jp.title) LIKE LOWER(CONCAT('%', :query, '%'))
               OR LOWER(jp.description) LIKE LOWER(CONCAT('%', :query, '%')))
         AND (:organizationId IS NULL OR jp.organization.id = :organizationId)
     """)
-    fun findJobPostings(
+    fun findJobPosts(
         @Param("query") query: String?,
         @Param("organizationId") organizationId: Long?,
         pageable: Pageable
-    ): Page<JobPosting>
+    ): Page<JobPost>
 }
