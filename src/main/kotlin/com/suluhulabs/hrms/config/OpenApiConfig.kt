6430e0898b90461934f12737484cd0dabb00effe
Alan Amalemba <alanlsamalemba@gmail.com>
2025-09-17 12:46:57 +0300

    feat: enable attatching of auth token in swagger ui docs

--- src/main/kotlin/com/suluhulabs/hrms/config/OpenApiConfig.kt
+++ src/main/kotlin/com/suluhulabs/hrms/config/OpenApiConfig.kt
@@ -1,14 +1,26 @@
 package com.suluhulabs.hrms.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class OpenApiConfig {
+    val securitySchemeName = "bearerAuth"
+
     @Bean
     fun customOpenApi(): OpenAPI = OpenAPI().info(
         Info().title("HRMS API documentation").version("1.0").description("API documentation for HRMS api server")
-    )
+    ).addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+        .components(
+            Components().addSecuritySchemes(
+                securitySchemeName, SecurityScheme().name(securitySchemeName).type(
+                    SecurityScheme.Type.HTTP
+                ).scheme("bearer").bearerFormat("JWT")
+            )
+        )
 }
